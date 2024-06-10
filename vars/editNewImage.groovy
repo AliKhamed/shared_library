47afb327a16ea45533e15dda24476ffee18fd3aa
Ali khames <46407148+AliKhamed@users.noreply.github.com>
2024-06-10 04:11:31 +0300

    Update editNewImage.groovy

--- vars/editNewImage.groovy
+++ vars/editNewImage.groovy
@@ -1,9 +1,29 @@
 #!/usr/bin/env groovy
 
-// KubernetesCredentialsID 'KubeConfig file'
-def call(String imageName) {
+def call(String githubToken, String imageName, String gitUserEmail, String gitUserName, String gitRepoName) {
+
+    withCredentials([string(credentialsId: "${githubToken}", variable: 'GITHUB_TOKEN')]) {
+                sh '''
+                    git config user.email "${gitUserEmail}"
+                    git config user.name "${gitUserName}"
+                    sed -i "s|image:.*|image: ${imageName}:${BUILD_NUMBER}|g" k8s/deployment.yaml
+                    git add k8s/deployment.yaml
+                    git commit -m "Update deployment image to version ${BUILD_NUMBER}"
+                    git push https://${GITHUB_TOKEN}@github.com/${gitUserName}/${gitRepoName} HEAD:test
+                '''
     
-    // Edit deployment.yaml with new Docker Hub image
-    sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' deployment.yml"
 
 }
+}
+
+
+
+// #!/usr/bin/env groovy
+
+// // KubernetesCredentialsID 'KubeConfig file'
+// def call(String imageName) {
+    
+//     // Edit deployment.yaml with new Docker Hub image
+//     sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' deployment.yml"
+
+// }
